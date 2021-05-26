1391831c29c2fe11d50522f5d466999bbf195e5b
DevBloo <devchanghwan@gmail.com>
2021-05-26 13:58:59 +0900

    feat: post, get 구현

--- app.py
+++ app.py
@@ -17,17 +17,28 @@ def home():
 ## API 역할을 하는 부분
 @app.route('/review', methods=['POST'])
 def write_review():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': '이 요청은 POST!'})
+    title = request.form['title']
+    author = request.form['author']
+    review = request.form['review']
+
+    print(title, author, review)
+
+    doc = {
+        'title': title,
+        'author': author,
+        'review': review
+    }
+
+    db.bookReview.insert_one(doc)
+
+    return jsonify({'msg': '등록 되었습니다.'})
 
 
 @app.route('/review', methods=['GET'])
 def read_reviews():
-    sample_receive = request.args.get('sample_give')
-    print(sample_receive)
-    return jsonify({'msg': '이 요청은 GET!'})
-
+    reviews = list(db.bookReview.find({}, {'_id': False}))
+    print(reviews)
+    return jsonify({'data': reviews})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
